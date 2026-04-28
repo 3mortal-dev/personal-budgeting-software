5c4390ab4ac14dba4f6a26ed136e94e15a65046a
Abdelhamid_ElRashidy <abdelhamidofficial2005@gmail.com>
2026-04-28 23:04:04 +0300

    main folder archeticture

---
+++ src/main/java/com/example/personal_budget/config/SecurityConfig.java
@@ -0,0 +1,5 @@
+package com.example.personal_budget.config;
+
+public class SecurityConfig {
+
+}
---
+++ src/main/java/com/example/personal_budget/dto/createTransactionRequest.java
@@ -0,0 +1,5 @@
+package com.example.personal_budget.dto;
+
+public class createTransactionRequest {
+
+}
