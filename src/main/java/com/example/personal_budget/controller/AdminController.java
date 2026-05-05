e66cfb0da15f811e29b2ffb30bc014fe5c54bde4
Abdelhamid_ElRashidy <abdelhamidofficial2005@gmail.com>
2026-05-06 01:22:29 +0300

    Admin controller endpoints added

--- src/main/java/com/example/personal_budget/controller/AdminController.java
+++ src/main/java/com/example/personal_budget/controller/AdminController.java
@@ -1,14 +1,45 @@
 package com.example.personal_budget.controller;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.example.personal_budget.service.UserService;
+
+
+@RestController
+@RequestMapping("/api/admin")
 public class AdminController {
 
+    // Functionalities needed:
+    // 1. View all users
+    // 2. Delete a user
+    // 3. View all transactions of a user (optional)
+    // 4. @GetMapping("path")
+
+    @Autowired
+    private UserService userService;
+
+    
     @GetMapping("/users")
-    public String getUsers() {
-        return "home";
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @DeleteMapping("/users/{id}")    
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        userService.deleteUserById(id);
+        return ResponseEntity.ok("User deleted");
     }
 
 }
