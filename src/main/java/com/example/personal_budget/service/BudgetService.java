1de2577b0e4a794107b8214d46ae316c5c8a63c1
Abdelhamid_ElRashidy <abdelhamidofficial2005@gmail.com>
2026-05-06 03:27:12 +0300

    Bugs Resolved & Fronted register connected demo

--- src/main/java/com/example/personal_budget/service/BudgetService.java
+++ src/main/java/com/example/personal_budget/service/BudgetService.java
@@ -1,5 +1,8 @@
 package com.example.personal_budget.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BudgetService {
 
     public void applyExpenseToBudget(Long userID, Long categoryID, double amount) {
--- src/main/java/com/example/personal_budget/service/DashboardService.java
+++ src/main/java/com/example/personal_budget/service/DashboardService.java
@@ -1,5 +1,8 @@
 package com.example.personal_budget.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class DashboardService {
 
 }
