c651efc7f6f757873253333072fc9002b8990f97
nerdev1 <youssefsaleh1120@gmail.com>
2026-05-06 22:41:03 +0300

    Add Category module

--- src/main/java/com/example/personal_budget/entity/Category.java
+++ src/main/java/com/example/personal_budget/entity/Category.java
@@ -1,26 +1,31 @@
 package com.example.personal_budget.entity;
 
+import com.example.personal_budget.enums.CategoryType;
+
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
-@Table(name = "categories")
 @Getter
 @Setter
 @Builder
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "categories")
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long categoryID;
+    private Long id;
 
-    private String name;
-
-    private String description;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = true)
     private User user;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CategoryType type;
+
+    @Column(nullable = false)
+    private String name;
 }
