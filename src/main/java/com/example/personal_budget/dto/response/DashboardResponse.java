feba81b46cd928c1a01d6f9e0a6fe6e8c7002a3d
Abdelhamid_ElRashidy <abdelhamidofficial2005@gmail.com>
2026-05-07 03:00:13 +0300

    edit dashbaordresponse

--- src/main/java/com/example/personal_budget/dto/response/DashboardResponse.java
+++ src/main/java/com/example/personal_budget/dto/response/DashboardResponse.java
@@ -1,16 +1,19 @@
-// package com.example.personal_budget.dto.response;
+package com.example.personal_budget.dto.response;
 
-// import java.math.BigDecimal;
+import java.math.BigDecimal;
+import java.util.List;
+import com.example.personal_budget.dto.response.TransactionDTO;
+import com.example.personal_budget.dto.response.GoalDTO;
+import com.example.personal_budget.dto.response.BudgetDTO;
 
-// import lombok.Data;
+import lombok.Data;
 
-// @Data
-// public class DashboardResponse {
-//     private Long userId;
-//     private BigDecimal totalBalance;
-//     private BigDecimal monthlyIncome;
-//     private BigDecimal monthlyExpense;
-//     private List<Transaction> recentTransactions;
-//     private List<Goal> goals;
-//     private List<Budget> budgets;
-// }
+@Data
+public class DashboardResponse {
+    private BigDecimal totalBalance;
+    private BigDecimal monthlyIncome;
+    private BigDecimal monthlyExpense;
+    private List<TransactionDTO> recentTransactions;
+    private List<GoalDTO> goals;
+    private List<BudgetDTO> budgets;
+}
