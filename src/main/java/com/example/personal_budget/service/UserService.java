5318dccfb44ea5879b0586249d12799715ff8fc4
3mortal <maymnahmd@gmail.com>
2026-05-07 12:24:15 +0300

    Add getUser method to UserService with the userId as a parameter

--- src/main/java/com/example/personal_budget/service/UserService.java
+++ src/main/java/com/example/personal_budget/service/UserService.java
@@ -1,51 +1,57 @@
 package com.example.personal_budget.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.personal_budget.entity.User;
+import com.example.personal_budget.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import com.example.personal_budget.entity.User;
-import com.example.personal_budget.repository.UserRepository;
-
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
 
-    private Long getUserIdByEmail(String email) {
-        return userRepository.findIdByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found with email: " + email));
+    private final UserRepository userRepository;
+
+    private Long getUserIdByEmail (String email) {
+        return userRepository.findIdByEmail(email).orElseThrow(
+                () -> new RuntimeException("User not found with email: " + email));
     }
 
-    private User getUserByEmail(String email) {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found with email: " + email));
+    private User getUserByEmail (String email) {
+        return userRepository.findByEmail(email).orElseThrow(
+                () -> new RuntimeException("User not found with email: " + email));
     }
 
-    public boolean userExistsByEmail(String email) {
+    public boolean userExistsByEmail (String email) {
         return userRepository.findByEmail(email).isPresent();
     }
 
     // get userid from userdetails
-    public long getUserId(UserDetails userDetails) {
+    public long getUserId (UserDetails userDetails) {
         String username = userDetails.getUsername();
         return getUserIdByEmail(username);
     }
 
-    // get userid from userdetails
-    public User getUser(UserDetails userDetails) {
+    // get user from userdetails
+    public User getUser (UserDetails userDetails) {
         String username = userDetails.getUsername();
         return getUserByEmail(username);
     }
 
-    public Iterable<User> getAllUsers() {
+    // get user by userId
+    public User getUser (Long userId) {
+        return userRepository.findById(userId).orElseThrow(
+                () -> new RuntimeException("User with id " + userId + "not found"));
+    }
+
+    public Iterable<User> getAllUsers () {
         return userRepository.findAll();
     }
 
-    public void deleteUserById(Long id) {
+    public void deleteUserById (Long id) {
         userRepository.deleteById(id);
     }
 
-    
+
 }
