e178feeb037981aa9011a38584693143f83c2e9a
Abdelhamid_ElRashidy <abdelhamidofficial2005@gmail.com>
2026-05-07 03:10:10 +0300

    added updateNotificationsetting function in userModule

--- src/main/java/com/example/personal_budget/repository/UserRepository.java
+++ src/main/java/com/example/personal_budget/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.personal_budget.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -13,5 +14,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u.id FROM User u WHERE u.email = :email")
     Optional<Long> findIdByEmail(@Param("email") String email);
+
+    @Modifying
+    @Query("UPDATE User u SET u.budgetAlertenabled = :budgetAlertEnabled, u.goalProgressAlertEnabled = :goalProgressAlertEnabled WHERE u.id = :id")
+    void updateNotificationSettingsById(@Param("id") Long id, @Param("budgetAlertEnabled") boolean budgetAlertEnabled, @Param("goalProgressAlertEnabled") boolean goalProgressAlertEnabled);
 
 }
