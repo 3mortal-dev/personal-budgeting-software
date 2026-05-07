aaff294f14d0ca8a72ced1b888ec4342294ca3dd
3mortal <maymnahmd@gmail.com>
2026-05-07 12:25:14 +0300

    Add getById method to CategoryService

--- src/main/java/com/example/personal_budget/service/CategoryService.java
+++ src/main/java/com/example/personal_budget/service/CategoryService.java
@@ -1,17 +1,15 @@
 package com.example.personal_budget.service;
 
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
-import lombok.RequiredArgsConstructor;
-
-import com.example.personal_budget.repository.CategoryRepository;
 import com.example.personal_budget.dto.request.CreateCategoryRequest;
 import com.example.personal_budget.entity.Category;
-import com.example.personal_budget.enums.CategoryType;
-import com.example.personal_budget.repository.UserRepository;
 import com.example.personal_budget.entity.User;
+import com.example.personal_budget.enums.CategoryType;
+import com.example.personal_budget.repository.CategoryRepository;
+import com.example.personal_budget.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,36 +18,36 @@ public class CategoryService {
     private final CategoryRepository categoryRepository;
     private final UserRepository userRepository;
 
-    public List<Category> getAllCategories(Long userID) {
+    public Category getById (Long categoryId) {
+        return categoryRepository.findById(categoryId).orElseThrow(
+                () -> new RuntimeException("Category with id " + categoryId + "not found"));
+    }
+
+    public List<Category> getAllCategories (Long userID) {
         return categoryRepository.findByUserIdOrType(userID, CategoryType.BUILT_IN);
     }
 
-    public List<Category> getBuiltInCategories() {
+    public List<Category> getBuiltInCategories () {
         return categoryRepository.findByType(CategoryType.BUILT_IN);
     }
 
-    public List<Category> getCustomCategories(Long userID) {
+    public List<Category> getCustomCategories (Long userID) {
         return categoryRepository.findByUserId(userID);
     }
 
-    public Category addCustomCategory(Long userID, CreateCategoryRequest request) {
+    public Category addCustomCategory (Long userID, CreateCategoryRequest request) {
 
-        User user = userRepository.findById(userID)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+        User user = userRepository.findById(userID).orElseThrow(() -> new RuntimeException("User not found"));
 
-        Category category = Category.builder()
-                .user(user)
-                .name(request.getName())
-                .type(request.getType())
-                .build();
+        Category category = Category.builder().user(user).name(request.getName()).type(request.getType()).build();
 
         return categoryRepository.save(category);
     }
 
-    public Category editCustomCategory(Long userID, Long categoryID, CreateCategoryRequest request) {
+    public Category editCustomCategory (Long userID, Long categoryID, CreateCategoryRequest request) {
 
-        Category category = categoryRepository.findByIdAndUserId(categoryID, userID)
-                .orElseThrow(() -> new RuntimeException("Category not found"));
+        Category category = categoryRepository.findByIdAndUserId(categoryID, userID).orElseThrow(
+                () -> new RuntimeException("Category not found"));
 
         category.setName(request.getName());
         category.setType(request.getType());
@@ -57,10 +55,10 @@ public class CategoryService {
         return categoryRepository.save(category);
     }
 
-    public void deleteCustomCategory(Long userID, Long categoryID) {
+    public void deleteCustomCategory (Long userID, Long categoryID) {
 
-        Category category = categoryRepository.findByIdAndUserId(categoryID, userID)
-                .orElseThrow(() -> new RuntimeException("Category not found"));
+        Category category = categoryRepository.findByIdAndUserId(categoryID, userID).orElseThrow(
+                () -> new RuntimeException("Category not found"));
 
         categoryRepository.delete(category);
     }
