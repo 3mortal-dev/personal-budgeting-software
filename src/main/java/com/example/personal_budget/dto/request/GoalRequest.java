0afc0d49720a088b988c3288462cd35457e1fb5a
most3f3.1112006@gmail.com <most3f3.1112006@gmail.com>
2026-05-08 17:50:24 +0300

    add Positive Validate at GoalRequest

--- src/main/java/com/example/personal_budget/dto/request/GoalRequest.java
+++ src/main/java/com/example/personal_budget/dto/request/GoalRequest.java
@@ -3,19 +3,22 @@ package com.example.personal_budget.dto.request;
 import lombok.Data;
 import java.time.LocalDate;
 import jakarta.validation.constraints.NotNull;
-
+import jakarta.validation.constraints.Positive;
 @Data
 public class GoalRequest {
 
     private String goalName;
 
+    @Positive(message = "Target amount must be positive")
     @NotNull(message = "Target amount is required")
     private Double targetAmount;
 
     @NotNull(message = "Deadline is required")
     private LocalDate deadline;
 
+    @Positive(message = "Saved amount must be positive")
     private Double savedAmount;
+    
     private String iconClass;
     private String iconColor;
 }
