ecc851801249289f657946d7251f06c2bf742ce2
3mortal <maymnahmd@gmail.com>
2026-05-09 01:36:52 +0300

    Refactor UserService to improve code structure and add transactional support for notification settings update

--- src/main/java/com/example/personal_budget/controller/NotificationController.java
+++ src/main/java/com/example/personal_budget/controller/NotificationController.java
@@ -1,26 +1,18 @@
 package com.example.personal_budget.controller;
 
 
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.personal_budget.dto.response.NotificationResponse;
 import com.example.personal_budget.entity.Notification;
 import com.example.personal_budget.enums.NotificationEventType;
 import com.example.personal_budget.service.NotificationService;
 import com.example.personal_budget.service.UserService;
-
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -36,13 +28,15 @@ public class NotificationController {
     @GetMapping("/all")
     public ResponseEntity<List<NotificationResponse>> getAllNotifications(@AuthenticationPrincipal UserDetails userDetails) {
         Long userId = userService.getUserId(userDetails);
-        List<Notification> notifications = notificationService.getUserNotifications(userId);   
+        List<Notification> notifications = notificationService.getUserNotifications(userId);
         List<NotificationResponse> response = ResponseMapper(notifications);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/recent?limit={limit}")
-    public ResponseEntity<List<NotificationResponse>> getRecentNotifications(@RequestParam int limit, @AuthenticationPrincipal UserDetails userDetails) {
+    @GetMapping("/recent")
+    public ResponseEntity<List<NotificationResponse>> getRecentNotifications(
+            @RequestParam int limit,
+            @AuthenticationPrincipal UserDetails userDetails) {
         Long userId = userService.getUserId(userDetails);
         List<Notification> notifications = notificationService.getReceivedNotifications(userId, limit);
         List<NotificationResponse> response = ResponseMapper(notifications);
@@ -60,16 +54,13 @@ public class NotificationController {
     @GetMapping("/type")
     public ResponseEntity<List<NotificationResponse>> getNotificationsByType(
             @RequestParam NotificationEventType type,
-            @AuthenticationPrincipal UserDetails userDetails
-    ) {
+            @AuthenticationPrincipal UserDetails userDetails) {
 
         Long userId = userService.getUserId(userDetails);
 
-        List<Notification> notifications =
-                notificationService.getNotificationsByType(userId, type);
+        List<Notification> notifications = notificationService.getNotificationsByType(userId, type);
 
-        List<NotificationResponse> response =
-                ResponseMapper(notifications);
+        List<NotificationResponse> response = ResponseMapper(notifications);
 
         return ResponseEntity.ok(response);
     }
